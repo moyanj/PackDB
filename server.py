a1ee665684237f54df685278f3e3f180ddc39bc3
moyan <1561515308@qq.com>
2024-12-05 22:36:21 +0800

    实现docker运行

--- server.py
+++ server.py
@@ -15,6 +15,10 @@ async def main(request: Request):
     results = await engine.search_async(query)
     return list(set(results))
 
+@app.get("/health")
+async def read_root():
+    return {"status": "ok"}
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
