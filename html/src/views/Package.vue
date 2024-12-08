6fd95c232c8e2a24c873db705e4d4adbadbb5c32
moyan <1561515308@qq.com>
2024-12-08 14:12:04 +0800

    update package page

--- html/src/views/Package.vue
+++ html/src/views/Package.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
 import { db } from '../data';
+import { ElCard, ElRow, ElCol } from 'element-plus';
 
 const route = useRoute();
 const name = route.params.name as string;
@@ -10,10 +11,28 @@ console.log(data)
 </script>
 
 <template>
-<h1>{{ $route.params.name }}</h1>
+    <el-row>
+        <el-card class="big-card summary">
+            <h3>{{ name }}</h3>
+        </el-card>
+    </el-row>
+    <el-row :gutter="0">
+        <el-col :span="6"><el-card class="big-card"></el-card></el-col>
+        <el-col :span="18"><el-card class="big-card"></el-card></el-col>
+    </el-row>
 
 </template>
 
 <style scoped>
+.big-card {
+    margin-top: 15px;
+    margin-left: 25px;
+    margin-right: 10px;
+    --el-card-padding: 15px;
+}
 
+.summary {
+    width: 100%;
+    height: 25vh;
+}
 </style>
